1a9230af00e9935dcf53865a1998c1870aa0b3bb
victorcodebase <victorkithinji@outlook.com>
2025-03-03 20:58:47 +0300

    categories immutable

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,16 +3,16 @@ const getCategories = () => {
 	message = req.query.message;
 	temp_res = { "server res: ": "server reached, the called function has not been implemented yet", "client message": message || "no message" };
 	res.status(500).json(temp_res);
-}
+};
 
 const addCategories = () => {
 	// TODO: implement addCategories method
 	message = req.query.message;
-	temp_res = { "server res: ": "server reached, the called function has not been implemented yet", "client message": message || "no message" };
-	res.status(500).json(temp_res);
-}
+	temp_res = { message: "Forbidden: You cannot add new categories" };
+	res.status(403).json(temp_res);
+};
 
 module.exports = {
-    getCategories,
-    addCategories
-}
+	getCategories,
+	addCategories,
+};
