5073a8b3847bbdfd87e4b8e511f659780fe0c8df
victorcodebase <victorkithinji@outlook.com>
2025-03-08 14:58:20 +0300

    b2 config

---
+++ src/config/b2Config.js
@@ -0,0 +1,11 @@
+const {S3Client} = require("@aws-sdk/client-s3")
+require("dotenv").config()
+
+const s3 = new S3Client({
+	endpoint: process.env.B2_ENDPOINT,
+	region: "us-west-004",
+	credentials: {
+		accessKeyId: process.env.B2_KEY_ID,
+		secretAccessKey: process.env.B2_APPLICATION_KEY,
+	},
+});
